fb8e47f32e9611c6b51cec693a8ee1c0dfd5bb81
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-01-23 11:08:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-define('$server','localhost');
+define('DB_SERVER','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_DATABASE','db_store');
