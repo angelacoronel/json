59494a8867b4420168c595c6bf9ef6570deecdb7
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-01-28 10:20:10 +0800

    Update json-2.php

--- json-2.php
+++ json-2.php
@@ -1,14 +1,21 @@
 <?php
-$json = file_get_contents('http://localhost/json/json-1.php');
+$json = file_get_contents('http://192.168.64.2/json/json-1.php');
 
 $data = json_decode($json,true);
-
-$list = $data['stocks'][1];
-//$list = $data['stocks'][0]['sto_name'];
-
-//var_dump($data);
-//echo "<pre>";
-
-print_r($list);
-
+$list = $data['Tracks'];
+//$list = $data['pets'][1];
+?>
+<h1> Tracks </h1>
+<?php
+foreach($list as $value){
+    ?>
+    <ul>
+        <h2><?php echo $value['title'];?></h2>
+        <li><?php echo $value['artist'];?></li>
+        <li><?php echo $value['genre'];?></li>
+        <li><?php echo $value['album'];?></li>
+    </ul>
+ 
+<?php
+}
 ?>
